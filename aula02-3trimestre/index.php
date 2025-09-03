5eb3fad7c23d533dc282deafff19afd4bc688f09
Henrike K. Carvalho <henrike.kcarvalho@aluno.edu.es.gov.br>
2025-09-03 11:58:46 -0300

    Create index.php

---
+++ aula02-3trimestre/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 02 PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome="Henrike";
+            echo "Meu nome é " .$nome ;
+        ?>
+
+    </body>
+
+</html>
