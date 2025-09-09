23e89aa5bd1e4dd2c7ed333aaf141abf7b3323e3
Henrike K. Carvalho <henrike.kcarvalho@aluno.edu.es.gov.br>
2025-09-09 11:54:10 -0300

    Create index.php

---
+++ aula-033ºTrimestre/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title> Aula 02 PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome="Henrike";
+            $num1=10;
+            $num2=2;
+            $soma=$num1+$num2;
+            
+
+
+
+
+            echo "Meu nome é " .$nome ; 
+            echo "<br>Meu nome é  $nome" ; 
+            echo "<br><br> A soma de $num1 + $num2 é igual a $soma"; 
+        ?>
+
+    </body>
+
+</html>
