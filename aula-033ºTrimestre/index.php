829a96cbe5ef04eaf56b52272a9ccbc339e48ae7
Henrike K. Carvalho <henrike.kcarvalho@aluno.edu.es.gov.br>
2025-09-10 11:54:00 -0300

    Update index.php

--- aula-033ºTrimestre/index.php
+++ aula-033ºTrimestre/index.php
@@ -1,23 +1,20 @@
 <html>
     <head>
-        <title> Aula 02 PHP</title>
+        <title> Aula 03 PHP</title>
     </head>
     <body>
         <?php
             $nome="Henrike";
-            $num1=10;
-            $num2=2;
-            $soma=$num1+$num2;
-            
-
-
-
-
+            $n1=10;
+            $n2=2;
+            $soma=$n1+$n2;
             echo "Meu nome é " .$nome ; 
             echo "<br>Meu nome é  $nome" ; 
-            echo "<br><br> A soma de $num1 + $num2 é igual a $soma"; 
+            echo "<br><br> A soma de $n1 + $n2 é igual a $soma";
+            echo "<br><br> O resultado da Subtração dos números $n1 e $n2 é igual a: " .($n1-$n2) ;
+            echo "<br><br> O número $n1 Multiplicado por $n2 é igual a: " .($n1*$n2) ;
+            echo "<br><br> O número $n1 divido por $n2 é: " .($n1/$n2) ;
+            echo "<br><br> O resto da divisão de $n1 por $n2 é: ".($n1%$n2) ;
         ?>
-
     </body>
-
 </html>
