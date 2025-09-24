75b53a35336c6f2e52f45cfe73bfcc0afc81840a
Henrike K. Carvalho <henrike.kcarvalho@aluno.edu.es.gov.br>
2025-09-24 14:08:09 -0300

    Create index.php

---
+++ aula05-3trimestre/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title> Aula 05 PHP</title>  
+    </head>
+    <body>
+        <?php
+            $num1= 2;
+            $num2= $_GET["x"]; 
+
+            echo "<br>Os valores informados são $num1 e $num2 ";
+            echo "<br><br>O valor absoluto de $num1 é " .abs($num1);
+            echo "<br><br>O número $num2 <sup>$num1</sup> é igual a " .pow($num2,$num1);
+            echo "<br><br>A raiz quadrada de $num2 é " .sqrt($num2);
+            echo "<br><br> O $num2 arredondado fica " .round($num2);
+            echo "<br> O $n2 arredondado para cima fica " .ceil($n2);
+            echo "<br> O $n2 arredondado para baixo fica " .floor($n2);
+         ?>
+    </body>
+</html>
